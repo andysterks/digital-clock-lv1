6072e5aad86bdcf06b7022c275a4b0781f865fde
Andy Sterkowitz <andy@andysterkowitz.com>
2021-01-12 11:03:05 -0800

    Adds Days module in separate js file

--- days.js
+++ days.js
@@ -1,3 +1,19 @@
 const Days = (function() {
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
 
+  function getDayByIndex(index) {
+    return days[index];
+  }
+
+  return {
+    getDayByIndex
+  }
 })();
